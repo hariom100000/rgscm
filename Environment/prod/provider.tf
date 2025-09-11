7d5814f1c33bcbd3b0f34041bab267efa5fd35f6
dharma <hariom100000@gmail.com>
2025-09-11 13:26:38 +0530

    adding

--- Environment/prod/provider.tf
+++ Environment/prod/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG-backend"
+    resource_group_name  = "RG_Backend"
     storage_account_name = "madarikakhel"           # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
