9b36c956fb177e55fd72b472ce64c7dd34370d09
hariom100000 <hariom100000@gmail.com>
2025-09-23 18:23:02 +0530

    Update provider.tf

--- Environment/prod/provider.tf
+++ Environment/prod/provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "azurerm" {
+ # backend "azurerm" {
     #resource_group_name  = "RG_Backend"
     #storage_account_name = "madarikakhel"
     #container_name       = "tfstate"    
     #key                  = "prod.terraform.tfstate" 
-  }
+ # }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
